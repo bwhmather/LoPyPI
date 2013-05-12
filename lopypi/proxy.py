f679c85700d4e71078e7e7e5f2917e05473a8457
Ben Mather <bwhmather@bwhmather.com>
2013-05-12 17:18:49 +0100

    Initial (read f'ugly) implementation of get_file function
    get_file reads file from local store if possible or downloads, caches
    and simultaneously streams from upstream url.

--- lopypi/proxy.py
+++ lopypi/proxy.py
@@ -1,8 +1,11 @@
 import os
 import mimetypes
+import tempfile
+import requests
+import shutil
 
 from flask import Blueprint, Flask, current_app, url_for
-from flask import render_template, send_file, abort
+from flask import render_template, send_file, abort, Response
 
 from lopypi.store import PackageStore
 from lopypi.pypi import PyPI
@@ -17,9 +20,9 @@ def list_packages():
     """
     pypi = current_app.config['pypi']
 
-    packages = (dict(name=name,
-                     uri=url_for('proxy.list_files', package=name))
-                for name in pypi.list_packages())
+    packages = list(pypi.list_packages())
+    for package in packages:
+        package['uri'] = url_for('proxy.list_files', package=package['name'])
     return render_template("package_list.html", packages=packages)
 
 
@@ -31,10 +34,11 @@ def list_files(package):
     store = current_app.config['cached_file_store']
 
     files = list(pypi.list_files(package))
-    map(lambda f: f.update({'uri': url_for('proxy.get_file',
-                                           package=package,
-                                           filename=f['filename'])}),
-        files)
+    for f in files:
+        f['uri'] = url_for('proxy.get_file',
+                           package=package,
+                           filename=f['filename'])
+
     return render_template("file_list.html",
                            package=package,
                            files=files)
@@ -47,25 +51,52 @@ def get_file(package, filename):
     pypi = current_app.config['pypi']
     store = current_app.config['cached_file_store']
 
-    f = store.get_file(package, filename)
-
-    if not f:
-        uri = pypi.get_file_uri(package, filename)
-        if not uri:
-            abort(404)
-        # TODO should be async
-        # TODO fix simultaneous downloads of same packae
-        # TODO give option of redirecting instead of saving
-        with urllib2.urlopen(uri) as download:
-            store.add_file(package, filename, download)
-
-        f = store.get_file(package, filename)
-
     content_type, _ = mimetypes.guess_type(filename)
     if content_type is None and filename.endswith(".egg"):
         content_type = "application/zip"
 
-    return send_file(f, mimetype=content_type)
+    f = store.get_file(package, filename)
+    if f:
+        # file is already cached locally.  Send it
+        return send_file(f, mimetype=content_type)
+
+    ## request file from pypi and stream it to client and disk
+    # get url of remote file
+    files = pypi.list_files(package)
+    file_info = filter(lambda f: f['filename'] == filename, files)
+    if not len(file_info):
+        abort(404)
+    file_info = file_info[0]
+    uri = file_info['remote_uri']
+
+    # start download
+    download = requests.get(uri, stream=True)
+    if download.status_code != 200:
+        # TODO
+        download.close()
+        abort(download.status_code)
+
+    def generate():
+        # TODO this is fucking ugly
+        data_iter = download.iter_content(1024)
+        outfile = tempfile.NamedTemporaryFile(delete=False)
+        try:
+            for data in data_iter:
+                outfile.write(data)
+                yield data
+        finally:
+            for data in data_iter:
+                outfile.write(data)
+            outfile.seek(0)
+
+            # TODO verify checksum
+            store.add_file(package, filename, outfile)
+
+    response = Response(generate(), mimetype=content_type)
+    if 'content-length' in download.headers:
+        response.content_length = download.headers['content-length']
+
+    return response
 
 
 def create_app(package_dir, pypi_url):
